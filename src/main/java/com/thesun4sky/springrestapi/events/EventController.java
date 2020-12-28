3db1c290dbcfba8d15d440a2c277fa0499dacc88
thesun4sky <thesun4sky@gmail.com>
2020-12-28 22:19:04 +0900

    Add(Event) - Add Event API Entity & Controller

---
+++ src/main/java/com/thesun4sky/springrestapi/events/EventController.java
@@ -0,0 +1,28 @@
+package com.thesun4sky.springrestapi.events;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Controller
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
+public class EventController {
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+
+        URI createURI = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+
+        return ResponseEntity.created(createURI).body(event);
+    }
+}
---
+++ src/main/java/com/thesun4sky/springrestapi/events/EventStatus.java
@@ -0,0 +1,6 @@
+package com.thesun4sky.springrestapi.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
